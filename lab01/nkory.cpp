f4f822a997fc47c6b274a480fcd4897ec8637690
Nicholas Kory <nmkory@ucdavis.edu>
2019-01-30 17:47:07 -0800

    Final
    Commented code, added debug flag to make file, removed extra test file

--- lab01/nkory.cpp
+++ lab01/nkory.cpp
@@ -1,14 +1,21 @@
+// Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu 
+// CSE 100 Spring Semester 2019
 #include <iostream>
 
 using namespace std;
 
-void printArray(const int* array, const int count)
+void printArray(const int* array, int count)
 {
-  for (int i = 0; i < count; i++) {
+  //print out the status of the array
+  for (int i = 0; i < count; i++)
+  {
     cout << array[i] << ";";
-  }
+  }  //array has been printed
+
+  //print a new line per instructions
   cout << endl;
-}
+}  //printArray()
+
 
 int main(int argc, char* argv[])
 {
@@ -18,34 +25,40 @@ int main(int argc, char* argv[])
   int count = 0;
   int* array;
 
+  //first cin is the length of the array to be sorted
   cin >> length;
   array = new int [length];
 
+  //start of insertion sort, insert first element into the array
   cin >> array[0];
   count++;
 
-  //for j = 2 to A.length
+  //for each additional element to be received
   for (int j = 1; j < length; j++)
   {
-    //key = A[j]
+    //take in the next element
     cin >> key;
 
-    //i = j - 1
+    //set i to the right most part of the sorted array
     i = j - 1;
 
+    //while we have not reached the end of the array and our new ele is smaller
     while (i >= 0 && array[i] > key)
     {
+      //move the element back one position and start while loop over
       array[i + 1] = array[i];
       i--;
-    }
+    }  //end of while loop means we found location for new element
 
+    //insert the element
     array[i + 1] = key;
 
+    //increment the count and print out the status of the array
     count++;
     printArray(array, count);
-  }
+  }  //end of insertion sort, all elements have been read and printed
 
-  //Free allocated space
+  //free allocated space
   delete[] array;
 
   return 0;
