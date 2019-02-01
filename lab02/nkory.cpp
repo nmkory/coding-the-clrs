19c20b026031e9addb808b302e0bdcabd6c2f78d
Nicholas Kory <nmkory@ucdavis.edu>
2019-02-01 15:18:02 -0800

    Load array working, mergesort stub added

--- lab02/nkory.cpp
+++ lab02/nkory.cpp
@@ -1,13 +1,13 @@
-// Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu 
+// Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu
 // CSE 100 Spring Semester 2019
 #include <iostream>
 
 using namespace std;
 
-void printArray(const int* array, int count)
+void printArray(const int* array, int length)
 {
   //print out the status of the array
-  for (int i = 0; i < count; i++)
+  for (int i = 0; i < length; i++)
   {
     cout << array[i] << ";";
   }  //array has been printed
@@ -17,46 +17,32 @@ void printArray(const int* array, int count)
 }  //printArray()
 
 
+void mergeSort(int* array, int p, int r)
+{
+  if (p < r)
+  {
+    int q = ((p + r)/2);
+    mergeSort(array, p, q);
+    mergeSort(array, q + 1, r);
+  }
+}  //mergeSort()
+
+
 int main(int argc, char* argv[])
 {
   int length;
-  int key;
-  int i;
-  int count = 0;
   int* array;
 
   //first cin is the length of the array to be sorted
   cin >> length;
   array = new int [length];
 
-  //start of insertion sort, insert first element into the array
-  cin >> array[0];
-  count++;
+  //load the unsorted array from input
+  for (int i = 0; i < length; i++)
+    cin >> array[i];
 
-  //for each additional element to be received
-  for (int j = 1; j < length; j++)
-  {
-    //take in the next element
-    cin >> key;
-
-    //set i to the right most part of the sorted array
-    i = j - 1;
-
-    //while we have not reached the end of the array and our new ele is smaller
-    while (i >= 0 && array[i] > key)
-    {
-      //move the element back one position and start while loop over
-      array[i + 1] = array[i];
-      i--;
-    }  //end of while loop means we found location for new element
-
-    //insert the element
-    array[i + 1] = key;
-
-    //increment the count and print out the status of the array
-    count++;
-    printArray(array, count);
-  }  //end of insertion sort, all elements have been read and printed
+  //print result
+  printArray(array, length);
 
   //free allocated space
   delete[] array;
