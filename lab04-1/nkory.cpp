758a00a3b8a6305f54753355c76352efbf151a26
Nicholas Kory <nmkory@ucdavis.edu>
2019-02-24 22:21:28 -0800

    Testing at 100

--- lab04-1/nkory.cpp
+++ lab04-1/nkory.cpp
@@ -1,110 +1,107 @@
 // Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu
 // CSE 100 Spring Semester 2019
 #include <iostream>
-#include <tuple>
 
 using namespace std;
 
-//returns tuple <index for max left, index for max right, sum max cross array>
-tuple <int,int,int32_t> findMaxCrossingSubarray(int32_t* array, int low,
-                                                int mid, int high)
+void printArray(const int* array, int length)
 {
-  int max_left = mid;
-  int max_right = mid;
-  int32_t left_sum = INT32_MIN;
-  int32_t right_sum = INT32_MIN;
-  int32_t sum = 0;
-
-  //find sum for left part of cross
-  for (int i = mid; i >= low; i--)
-  {
-    //track running left sum
-    sum = sum + array[i];
-
-    //if the new sum is greater than old, update with value and index
-    if (sum > left_sum)
-    {
-      left_sum = sum;
-      max_left = i;
-    }  //left value and index updated
-  } //the left sum and index have been found
-
-  //reset sum
-  sum = 0;
-
-  //find sum for right part of cross
-  for (int j = mid + 1; j <= high; j++)
-  {
-    //track running right sum
-    sum = sum + array[j];
-
-    //if the new sum is greater than old, update with value and index
-    if (sum > right_sum)
-    {
-      right_sum = sum;
-      max_right = j;
-    }  //right value and index updated
-  } //the right sum and index have been found
-
-  //add the left and right for total value
-  return (make_tuple (max_left, max_right, left_sum + right_sum));
-}  //findMaxCrossingSubarray()
+  //print out the status of the array
+  for (int i = 0; i < length; i++)
+    cout << array[i] << ";";
+}  //printArray()
 
 
-//returns tuple <index for max left, index for max right, sum max subarray>
-tuple <int,int,int32_t> findMaximumSubarray(int32_t* array, int low, int high)
+int left(int i)
 {
-  //base case: only one element, return the value of that one element
-  if (high == low)
-    return (make_tuple (low, high, array[low]));
+  //if root
+  if (i == 0)
+    return 1;
 
-  //need to recurse down and check the cross subarray for this array
   else
+    return ((2*i) + 1);
+}  //left()
+
+
+int right(int i)
+{
+  //if root
+  if (i == 0)
+    return 2;
+
+  else
+    return ((2*i) + 2);
+}  //right()
+
+
+void maxHeapify(int* array, int i, int heap_size)
+{
+  int largest;
+  int temp;
+  int l = left(i);
+  int r = right(i);
+
+  if ((l < heap_size) && (array[l] > array[i]))
+    largest = l;
+
+  else
+    largest = i;
+
+  if ((r < heap_size) && (array[r] > array[largest]))
+    largest = r;
+
+  if (largest != i)
   {
-    int mid = ((low + high) / 2);
+    temp = array[i];
+    array[i] = array[largest];
+    array[largest] = temp;
+    maxHeapify(array, largest, heap_size);
+  }
+}  //maxHeapify()
 
-    tuple <int, int, int32_t> left =
-      findMaximumSubarray(array, low, mid);
 
-    tuple <int, int, int32_t> right =
-      findMaximumSubarray(array, mid + 1, high);
+void buildMaxHeap(int* array, int length, int heap_size)
+{
+  int i = (length / 2) - 1;
 
-    tuple <int32_t, int32_t, int32_t> cross =
-      findMaxCrossingSubarray(array, low, mid, high);
+  if (i < 0)
+    i = 0;
 
-    //if the left-hand side has the greatest sum
-    if (get<2>(left) >= get<2>(right) && get<2>(left) >= get<2>(cross))
-      return left;
-
-    //else if the right-hand side has the greatest sum
-    else if (get<2>(right) >= get<2>(left) && get<2>(right) >= get<2>(cross))
-      return right;
-
-    //else it must be the max cross subarray
-    else
-      return cross;
-  }  //end of recursions and checking subarray
-}  //findMaximumSubarray()
+  for ( ; i >= 0; i--)
+    maxHeapify(array, i, heap_size);
+}  //buildMaxHeap()
 
 
 int main(int argc, char* argv[])
 {
   int length;
-  int32_t* array;
+  int heap_size;
+  int temp;
+  int* array;
 
   //first cin is the length of the array to be sorted
   cin >> length;
-  array = new int32_t [length];
+  heap_size = length;
+  array = new int [length];
 
-  //load the array of deltas from input, do not sort
+  //load the unsorted array from input
   for (int i = 0; i < length; i++)
     cin >> array[i];
 
-  //divide and conquer for max subarray indicies and value
-  tuple <int, int, int32_t> maxSubarray =
-    findMaximumSubarray(array, 0, length - 1);
+  //build the max heap
+  buildMaxHeap(array, length, heap_size);
 
-  cout << get<2>(maxSubarray);
+  //heapsort
+  for (int i = length - 1; i > 0; i--)
+  {
+    temp = array[0];
+    array[0] = array[i];
+    array[i] = temp;
+    maxHeapify(array, 0, --heap_size);
+  }  //the maxheap has now been sorted
+
+  //print result
+  printArray(array, length);
 
   //free allocated space
   delete[] array;
