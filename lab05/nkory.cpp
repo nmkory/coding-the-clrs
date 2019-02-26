ead361be15364ba4bbe0fb389b3d1e210c2cb69d
Nicholas Kory <nmkory@ucdavis.edu>
2019-02-26 00:02:44 -0800

    Added read and print

--- lab05/nkory.cpp
+++ lab05/nkory.cpp
@@ -2,6 +2,7 @@
 // CSE 100 Spring Semester 2019
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -23,41 +24,57 @@ int main(int argc, char* argv[])
   int length;
   int key;
   int i;
+  int j;
+  int k;
   int count = 0;
-  int* array;
+  vector<int>* array;
+  int temp;
 
   //first cin is the length of the array to be sorted
   cin >> length;
-  array = new int [length];
+  array = new vector<int> [length];
 
-  //start of insertion sort, insert first element into the array
-  cin >> array[0];
-  count++;
-
-  //for each additional element to be received
-  for (int j = 1; j < length; j++)
+  for (j = 0; j < length; j++)
   {
-    //take in the next element
-    cin >> key;
-
-    //set i to the right most part of the sorted array
-    i = j - 1;
-
-    //while we have not reached the end of the array and our new ele is smaller
-    while (i >= 0 && array[i] > key)
+    for (k = 0; k < 10; k++)
     {
-      //move the element back one position and start while loop over
-      array[i + 1] = array[i];
-      i--;
-    }  //end of while loop means we found location for new element
+      cin >> temp;
+      array[j].push_back(temp);
+    }
+  }
 
-    //insert the element
-    array[i + 1] = key;
+  // //for each additional element to be received
+  // for (int j = 1; j < length; j++)
+  // {
+  //   //take in the next element
+  //   cin >> key;
+  //
+  //   //set i to the right most part of the sorted array
+  //   i = j - 1;
+  //
+  //   //while we have not reached the end of the array and our new ele is smaller
+  //   while (i >= 0 && array[i] > key)
+  //   {
+  //     //move the element back one position and start while loop over
+  //     array[i + 1] = array[i];
+  //     i--;
+  //   }  //end of while loop means we found location for new element
+  //
+  //   //insert the element
+  //   array[i + 1] = key;
+  //
+  //   //increment the count and print out the status of the array
+  //   count++;
+  //   printArray(array, count);
+  // }  //end of insertion sort, all elements have been read and printed
 
-    //increment the count and print out the status of the array
-    count++;
-    printArray(array, count);
-  }  //end of insertion sort, all elements have been read and printed
+  for (j = 0; j < length; j++)
+  {
+    for (vector<int>::iterator it = array[j].begin(); it != array[j].end(); ++it)
+      cout << *it << ";";
+
+    cout << endl;
+  }
 
   //free allocated space
   delete[] array;
