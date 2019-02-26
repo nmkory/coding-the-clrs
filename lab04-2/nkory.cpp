b90c29d94f5aec66de5de801ad183f2450a4a860
Nicholas Kory <nmkory@ucdavis.edu>
2019-02-25 16:02:43 -0800

    Implemented randQuick
    Changed to use random quicksort, testing at 100%

--- lab04-2/nkory.cpp
+++ lab04-2/nkory.cpp
@@ -1,6 +1,7 @@
 // Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu
 // CSE 100 Spring Semester 2019
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ void printArray(const int* array, int length)
 
 int partition(int* array, int p, int r)
 {
+  //pivot is set to rightmost element
   int x = array[r];
   int i = p - 1;
   int temp;
@@ -37,21 +39,43 @@ int partition(int* array, int p, int r)
 }  //partition()
 
 
-void quicksort(int* array, int p, int r)
+int randomizedPartition(int* array, int p, int r)
 {
+  int i;  //random pivot index
+  int temp;  //temp var for inplace swapping
+
+  //choose a random i between and including p and r
+  i = rand() % (r - p + 1) + p;
+
+  //exchange array[r] with array[i] to set rand pivot
+  temp = array[i];
+  array[i] = array[r];
+  array[r] = temp;
+
+  //call partition with the rand pivot
+  return partition(array, p, r);
+}  //randomizedPartition()
+
+
+void randomizedQuicksort(int* array, int p, int r)
+{
+  //if we are not at base case
   if (p < r)
   {
-    int q = partition(array, p, r);
-    quicksort(array, p, q - 1);
-    quicksort(array, q + 1, r);
+    int q = randomizedPartition(array, p, r);
+    randomizedQuicksort(array, p, q - 1);
+    randomizedQuicksort(array, q + 1, r);
   }
-}  //quicksort()
+}  //randomizedQuicksort()
 
 
 int main(int argc, char* argv[])
 {
   int length;
   int* array;
+
+  //seed rand with the current time
+  srand(time(NULL));
 
   //first cin is the length of the array to be sorted
   cin >> length;
@@ -62,7 +86,7 @@ int main(int argc, char* argv[])
     cin >> array[i];
 
   //sort the array
-  quicksort(array, 0, length - 1);
+  randomizedQuicksort(array, 0, length - 1);
 
   //print result
   printArray(array, length);
