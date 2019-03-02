d9e9149daf633f6f6b75486e79201414f2566ce4
Nicholas Kory <nmkory@ucdavis.edu>
2019-03-02 13:55:06 -0800

    Removing vectors and using 2D array

--- lab05/nkory.cpp
+++ lab05/nkory.cpp
@@ -1,11 +1,10 @@
 // Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu
 // CSE 100 Spring Semester 2019
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-void loadArray(vector<int>* array, int length)
+void loadArray(int** array, int length)
 {
   int j;
   int k;
@@ -13,22 +12,23 @@ void loadArray(vector<int>* array, int length)
 
   for (j = 0; j < length; j++)
   {
+    array[j] = new int [10];
+
     for (k = 0; k < 10; k++)
-    {
-      cin >> temp;
-      array[j].push_back(temp);
-    }
+      cin >> array[j][k];
   }
 }  //loadArray()
 
 
-void printArray(vector<int>* array, int length)
+void printArray(int** array, int length)
 {
-  for (int j = 0; j < length; j++)
+  int j;
+  int k;
+
+  for (j = 0; j < length; j++)
   {
-    for (vector<int>::iterator it = array[j].begin(); it != array[j].end();
-         ++it)
-      cout << *it << ";";
+    for (k = 0; k < 10; k++)
+      cout << array[j][k] << ";";
 
     cout << endl;
   }
@@ -41,44 +41,18 @@ int main(int argc, char* argv[])
   int key;
   int i;
   int count = 0;
-  vector<int>* array;
+  int** array;
 
-  //first cin is the length of the array to be sorted
+  //first cin is the length of the array to be sorted and load the 2D array
   cin >> length;
-  array = new vector<int> [length];
-
-
+  array = new int* [length];
   loadArray(array, length);
-
-
-  // //for each additional element to be received
-  // for (int j = 1; j < length; j++)
-  // {
-  //   //take in the next element
-  //   cin >> key;
-  //
-  //   //set i to the right most part of the sorted array
-  //   i = j - 1;
-  //
-  //   //while we have not reached the end of the array and our new ele is smaller
-  //   while (i >= 0 && array[i] > key)
-  //   {
-  //     //move the element back one position and start while loop over
-  //     array[i + 1] = array[i];
-  //     i--;
-  //   }  //end of while loop means we found location for new element
-  //
-  //   //insert the element
-  //   array[i + 1] = key;
-  //
-  //   //increment the count and print out the status of the array
-  //   count++;
-  //   printArray(array, count);
-  // }  //end of insertion sort, all elements have been read and printed
 
   printArray(array, length);
 
   //free allocated space
+  for (int j = 0; j < length; j++)
+    delete[] array[j];
   delete[] array;
 
   return 0;
