2e55929995638ac8628a490de9b60a1be480f925
Nicholas Kory <nmkory@ucdavis.edu>
2019-03-03 13:40:35 -0800

    Adding comments and cleaning code

--- lab05/nkory.cpp
+++ lab05/nkory.cpp
@@ -37,44 +37,50 @@ void printArray(int** array, int length)
 
 void countingSort(int** array, int** temp_array, int radix_digit, int length)
 {
-  int i;
-  int j;
-  int count_array[10];
+  int i;  //for loop index variable
+  int j;  //for loop index variable
+  int count_array[10];  //new array to hold freq of input elements
 
+  //initialize count array to 0s
   for (i = 0; i < 10; i++)
     count_array[i] = 0;
 
+  //count freq of elements by index (eg index 0 holds how many 0s there are)
   for (j = 0; j < length; j++)
     count_array[array[j][radix_digit]]++;
 
+  //adds input elements to calculate stable sorting index for sort
   for (i = 1; i < 10; i++)
     count_array[i] = count_array[i] + count_array[i-1];
 
+  //start sort from right to left
   for (j = length - 1; j >= 0; j--)
   {
+    //move into temp_array index the right most index of a value (offset by 1)
     temp_array[count_array[array[j][radix_digit]] - 1] = array[j];
+    //decrement that index in the count array
     count_array[array[j][radix_digit]]--;
-  }
+  }  //temp array is now properly sorted based on the radix_digit
 
+  //reset the pointers in array so array now holds the correct sorted order
   for (i = 0; i < length; i++)
-    array[i] = temp_array[i];
+   array[i] = temp_array[i];
 }  //countingSort()
 
 
 int main(int argc, char* argv[])
 {
   int length;
-  int key;
   int radix_digit;
   int** array;
   int** temp_array;
 
   //first cin is the length of the array to be sorted and load the 2D array
   cin >> length;
-  array = new int* [length];
+  array = new int* [length];  //array of pointers for loaded values
   loadArray(array, length);
 
-  temp_array = new int* [length];
+  temp_array = new int* [length];  //array of pointers for counting sort
 
   //radix sort using countingSort
   for(radix_digit = 9; radix_digit >=0; radix_digit--)
