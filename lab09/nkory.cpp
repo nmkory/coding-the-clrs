99f49266a7fbcd80e2f72b9fb47a1234ff1a724d
Nicholas Kory <nmkory@ucdavis.edu>
2019-04-13 13:14:00 -0700

    Queue working

--- lab09/nkory.cpp
+++ lab09/nkory.cpp
@@ -5,19 +5,55 @@
 
 using namespace std;
 
-struct node {
-  node* left;
-  node* right;
-  node* freq;
-  char c;
+class Node {
+  public:
+    Node* left{NULL};
+    Node* right{NULL};
+    int freq{0};
+    char c{'#'};
+    Node() {}
+    Node(Node* leftNode, Node* rightNode, int charFreq, char character)
+      : left (leftNode), right (rightNode), freq (charFreq), c (character) {}
 };
 
+bool operator<(const Node& lhs, const Node& rhs) {
+  return lhs.freq < rhs.freq;
+}
 
+bool operator>(const Node& lhs, const Node& rhs) {
+  return lhs.freq > rhs.freq;
+}
+
+bool operator==(const Node& lhs, const Node& rhs) {
+  return lhs.freq == rhs.freq;
+}
+
+bool operator!=(const Node& lhs, const Node& rhs) {
+  return lhs.freq != rhs.freq;
+}
 
 
 int main(int argc, char* argv[])
 {
-  priority_queue<node> queue ();
+  priority_queue<Node, vector<Node>, greater<vector<Node>::value_type>> queue;
+
+  Node n1;
+  Node n2;
+  Node n3;
+
+  n1.freq = 1;
+  n2.freq = 2;
+  n3.freq = 3;
+
+  if (n1 < n2)
+    std::cout << "Works" << '\n';
+
+  queue.push(n3);
+  queue.push(n1);
+  queue.push(n2);
+
+  cout << queue.top().freq << '\n';
+
 
   return 0;
 }  //main()
