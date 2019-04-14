ada2494074a3e8e04748ccc5e0732cbafabd65cf
Nicholas Kory <nmkory@ucdavis.edu>
2019-04-14 01:12:43 -0700

    Adding some comments

--- lab09/nkory.cpp
+++ lab09/nkory.cpp
@@ -1,5 +1,6 @@
 // Author: Nicholas Kory, SID# 100319778, nkory@ucmerced.edu
 // CSE 100 Spring Semester 2019
+// I did not work with anyone on this assignment.
 #include <iostream>
 #include <queue>
 #include <string>
@@ -8,26 +9,29 @@ using namespace std;
 
 class Node {
   public:
-    Node* left{NULL};
-    Node* right{NULL};
-    int freq{0};
-    char c{'#'};
+    Node* left {NULL};
+    Node* right {NULL};
+    int freq {0};
+    char c {'#'};
 
     Node() {}
     Node(Node* leftNode, Node* rightNode, int charFreq, char character)
       : left (leftNode), right (rightNode), freq (charFreq), c (character) {}
     ~Node() {}
 
-
-    bool operator() (const Node* lhs, const Node* rhs) const {
+    //overloaded () operator to organize min priority_queue
+    bool operator() (const Node* lhs, const Node* rhs) const
+    {
       return lhs->freq > rhs->freq;
     }  //operator()
 
-
-    void encode(string huffman_code, string* cipher) {
+    //used to encode the huffman strings after running algo
+    void encode(string huffman_code, string* cipher)
+    {
       if (left != NULL)
         left->encode(huffman_code + "0", cipher);
 
+      //ascii offset by 65 assumes we start with "A"
       if (c != '#')
          cipher[c - 65] = huffman_code;
 
@@ -36,19 +40,20 @@ class Node {
     }  //encode()
 
 
+    //clean tree used to free leaked memory from Node allocations
     void cleanTree()
     {
       if (left)
       {
         left->cleanTree();
         delete left;
-      } // if node left
+      } //if node left
 
       if (right)
       {
         right->cleanTree();
         delete right;
-      } // if node right
+      } //if node right
     } //cleanTree()
 };  //class Node
 
@@ -63,18 +68,24 @@ int main(int argc, char* argv[])
   string* cipher = new string[n];
   priority_queue<Node*, vector<Node*>, Node> queue;
 
-  for (i = 1, c = 'A'; i <= n; i++, c++) {
+  //read in char freq from stdin and push onto priority_queue
+  for (i = 1, c = 'A'; i <= n; i++, c++)
+  {
     cin >> c_freq;
     z = new Node(NULL, NULL, c_freq, c);
     queue.push(z);
-  }
+  }  //char nodes are organized in the priority_queue
 
-  for (i = 1; i <= (n - 1); i++) {
+  for (i = 1; i <= (n - 1); i++)
+  {
     z = new Node();
+
     z->left = queue.top();
     queue.pop();
+
     z->right = queue.top();
     queue.pop();
+
     z->freq = z->left->freq + z->right->freq;
     queue.push(z);
   }
@@ -83,9 +94,8 @@ int main(int argc, char* argv[])
   queue.pop();
   z->encode("", cipher);
 
-  for (i = 0, c = 'A'; i < n; i++, c++) {
-    std::cout << c << ":" << cipher[i] << '\n';
-  }
+  for (i = 0, c = 'A'; i < n; i++, c++)
+    cout << c << ":" << cipher[i] << '\n';
 
   z->cleanTree();
   delete z;
