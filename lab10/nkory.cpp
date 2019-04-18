b715ffe596d8bd296f009ecb31235b46df3323a3
Nicholas Kory <nmkory@ucdavis.edu>
2019-04-17 23:49:17 -0700

    Completed insert

--- lab10/nkory.cpp
+++ lab10/nkory.cpp
@@ -9,35 +9,15 @@ using namespace std;
 
 class Node {
   public:
+    Node* parent {NULL};
     Node* left {NULL};
     Node* right {NULL};
-    int freq {0};
-    char c {'#'};
+    int key {0};
 
     Node() {}
-    Node(Node* leftNode, Node* rightNode, int charFreq, char character)
-      : left (leftNode), right (rightNode), freq (charFreq), c (character) {}
+    Node(Node* parent, Node* leftNode, Node* rightNode, int key)
+      : parent(parent), left (leftNode), right (rightNode), key (key) {}
     ~Node() {}
-
-    //overloaded () operator to organize min priority_queue
-    bool operator() (const Node* lhs, const Node* rhs) const
-    {
-      return lhs->freq > rhs->freq;
-    }  //operator()
-
-    //used to encode the huffman strings after running algo
-    void encode(string huffman_code, string* cipher)
-    {
-      if (left != NULL)
-        left->encode(huffman_code + "0", cipher);
-
-      //ascii offset by 65 assumes we start with "A"
-      if (c != '#')
-         cipher[c - 65] = huffman_code;
-
-      if (right != NULL)
-        right->encode(huffman_code + "1", cipher);
-    }  //encode()
 
 
     //clean tree used to free leaked memory from Node allocations
@@ -58,52 +38,68 @@ class Node {
 };  //class Node
 
 
+class BST {
+  public:
+    Node* root {NULL};
+
+    BST() {}
+    BST(Node* root) : root (root) {}
+
+    void treeInsert(Node* z)
+    {
+      Node* y = NULL;
+      Node* x = root;
+
+      while (x != NULL)
+      {
+        y = x;
+        if (z->key < x->key)
+          x = x->left;
+
+        else
+          x = x->right;
+      }
+
+      z->parent = y;
+
+      if (y == NULL)
+        root = z; //tree was empty
+
+      else if (z->key < y->key)
+        y->left = z;
+
+      else
+        y->right = z;
+    }
+
+
+};
+
+
 int main(int argc, char* argv[])
 {
-  char c;
-  int c_freq;
-  int i;
-  int n = 6;
+  char command;
+  int key;
+  bool incoming_input = true;
   Node* z;
-  string* cipher = new string[n];
-  priority_queue<Node*, vector<Node*>, Node> queue;
+  BST* tree = new BST();
 
-  //read in char freq from stdin and push onto priority_queue
-  for (i = 1, c = 'A'; i <= n; i++, c++)
-  {
-    cin >> c_freq;
-    z = new Node(NULL, NULL, c_freq, c);
-    queue.push(z);
-  }  //char nodes are organized in the priority_queue
-
-  //huffman algorithm
-  for (i = 1; i <= (n - 1); i++)
-  {
-    z = new Node();
-
-    z->left = queue.top();
-    queue.pop();
-
-    z->right = queue.top();
-    queue.pop();
-
-    z->freq = z->left->freq + z->right->freq;
-    queue.push(z);
-  }  //z is ready to be encoded
-
-  //pop last node from queue and load huffman codes into cipher
-  z = queue.top();
-  queue.pop();
-  z->encode("", cipher);
-
-  //stdout the huffman codes
-  for (i = 0, c = 'A'; i < n; i++, c++)
-    cout << c << ":" << cipher[i] << '\n';
-
-  //free allocated memory
-  z->cleanTree();
-  delete z;
-  delete [] cipher;
+  //while there is still input commands
+  while (incoming_input) {
+    cin >> command;
+    switch (command) {
+      case 'i':
+        cin >> key;
+        z = new Node (NULL, NULL, NULL, key);
+        tree->treeInsert(z);
+        break;
+      case 'e' :
+        incoming_input = false;
+        break;
+      default :
+        incoming_input = false;
+    }  //end of switch
+  }  //after while loop, we have finished with this hash table
 
   return 0;
 }  //main()
